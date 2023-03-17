930d6e6b9feb8b4867c8e07d93df52257d09c414
Alec Hannaford <alec322@gmail.com>
2023-03-16 18:38:53 -0700

    my pic

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,6 +1,11 @@
 function AboutMe(props){
     return(
         <>
+        <div className="alec">
+            <img src="https://i.imgur.com/pu0GHYa.jpg"
+            height='200px'
+            ></img>
+        </div>
         <p>As a full-stack software engineer with a background in European history and medical devices, I bring a unique perspective to every project. My passion for improving processes and taking initiative sets me apart as an asset to any team. With a strong track record of success in fast-paced environments, I thrive on collaboration and am eager to contribute my expertise to your organization as a full-stack software engineer.</p>
         </>
     )
