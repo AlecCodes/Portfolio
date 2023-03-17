6501c3070e501374802949751f8384e38f97f4fb
Alec Hannaford <alec322@gmail.com>
2023-03-16 18:21:18 -0700

    rough draft

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
         <tr>
           <td></td>
           <td></td>
-          <td><div>alec hannaford</div></td>
+          <td><h1 className='title'>Alec Hannaford</h1>
+          </td>
         </tr>
         <tr>
           <td width='100px'></td>
@@ -24,16 +25,22 @@ function App() {
             href = "https://docs.google.com/document/d/1_XNrFfHQknI9V7kCpJt5JAXMfq8cJEaJeYNhISeBV4w/edit#heading=h.gjdgxs"
             target = "_blank"
             >resume</a>
-            <Link to = "/Contact">contact</Link>
+            {/* <Link to = "/Contact">contact</Link> */}
             <span>
             <a 
             href='https://www.linkedin.com/in/alec-hannaford/'
             target = "_blank"
             ><img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'></img></a>
+            <br></br>
             <a 
             href='https://github.com/AlecCodes'
             target = "_blank"
             ><img src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png'></img></a>
+            <br></br>
+            <a 
+            href='mailto: alec322@gmail.com'
+            target = "_blank"
+            ><img src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png'></img></a>
             </span>
           </td>
           <td className='Body'><Outlet/></td>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,20 @@
 function Contact(props){
-    return <h1>CONTACT</h1>
+    return (
+        <div>
+            <div className="Links">
+                <span>
+                <a 
+                href='https://www.linkedin.com/in/alec-hannaford/'
+                target = "_blank"
+                ><img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'></img></a>
+                <a 
+                href='https://github.com/AlecCodes'
+                target = "_blank"
+                ><img src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png'></img></a>
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
