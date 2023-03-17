2acb5fae9b256f2167ce7bcf5b213360cf2c1308
Alec Hannaford <alec322@gmail.com>
2023-03-16 17:47:37 -0700

    skills page

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,8 +1,27 @@
 function Skills(props){
     return(
-        <>
-            <h1>Skills</h1>
-        </>
+        <table border='solid'>
+            <tr>
+                <td>Backend Frameworks</td>
+                <td>Django, Express, SpringBoot, Node</td>
+            </tr>
+            <tr>
+                <td>Frontend</td>
+                <td>React, React Router, SCSS</td>
+            </tr>
+            <tr>
+                <td>Languages</td>
+                <td>Java, Python, Javascript, SQL, HTML, CSS</td>
+            </tr>
+            <tr>
+                <td>Database</td>
+                <td>PostgreSQL, Heroku Postgres, MongoDB, Mongoose, Bit.io, Django ORM</td>
+            </tr>
+            <tr>
+                <td>Misc</td>
+                <td>Heroku, Render.com, EJS, Maven, JWT, Express-session, bcrypt, psycopg, Git</td>
+            </tr>
+        </table>
     )
 }
 export default Skills
