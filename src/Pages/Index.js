729e4f663482e3988d21aa4efec38ffa23c136e1
Alec Hannaford <alec322@gmail.com>
2023-03-16 17:33:42 -0700

    projects formatted

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -2,7 +2,7 @@ import projects from '../projects.json'
 
 function Index(props){
     console.log(projects)
-    return (<>
+    return (<div>
     {projects.map((element, index) => {
         return (
             <div 
@@ -10,15 +10,15 @@ function Index(props){
             className="project"
             >
                 <h3>{element.name}</h3>
-                <a href = {element.url}>Deployed Site</a>
+                <a href = {element.url}>Deployed Site</a> • <a href={element.frontend}>GitHub Repo</a>
                 <div className='imgContainer'>
-                    <img src = {element.img}></img>
+                    <img src = {element.img} className={element.name}></img>
                 </div>
             </div>
         )
     })}
 
-    </>)
+    </div>)
 }
 
 export default Index
