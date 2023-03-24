639ea65aea1d3ab2642314cc7585c803c1007ce5
Alec Hannaford <alec322@gmail.com>
2023-03-23 22:30:37 -0700

    changed the links contact

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -11,9 +11,9 @@ function AboutMe(props){
         <p>As a <span>full-stack software engineer</span> with a background in European history and medical devices, I bring a unique perspective to every project. My passion for improving processes and taking initiative sets me apart as an asset to any team. With a strong track record of success in fast-paced environments, I thrive on collaboration and am eager to contribute my expertise to your organization as a full-stack software engineer.</p>
         <div className='contactme'>
             <h3>Contact me!</h3>
-            <a href='https://www.linkedin.com/in/alec-hannaford/'>Linkedin</a>
+            <a href='https://www.linkedin.com/in/alec-hannaford/'>https://www.linkedin.com/in/alec-hannaford/</a>
             <br/> 
-            <a href='mailto: alec322@gmail.com'>Gmail</a> 
+            <a href='mailto: alec322@gmail.com'>alec322@gmail.com</a> 
         </div>
         </>
     )
